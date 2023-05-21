65428d0a1756aee0e259d97f21e1f2726c64f612
ishibashm <ishibashm@gmail.com>
2023-05-21 21:08:59 +0900

    Cange line chart to candle

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request
 import yfinance as yf
-import matplotlib.pyplot as plt
+import mplfinance as mpf
 from io import BytesIO
 import base64
 
@@ -16,20 +16,18 @@ def roe():
     ticker = yf.Ticker(symbol)
     roe = ticker.info['returnOnEquity']
 
-    # 株価データの取得とチャート作成
+    # 株価データの取得
     history = ticker.history(period='1y')
-    plt.plot(history.index, history['Close'])
-    plt.xlabel('Date')
-    plt.ylabel('Price')
-    plt.title('Stock Price Chart for ' + symbol)
-    plt.grid(True)
 
+    # キャンドルチャートを描画
+    fig, ax = mpf.plot(history, type='candle', mav=(5, 10), volume=True, returnfig=True)
+    
     # チャートを画像に変換
     chart_img = BytesIO()
-    plt.savefig(chart_img, format='png')
+    fig.savefig(chart_img, format='png')
     chart_img.seek(0)
     chart_base64 = base64.b64encode(chart_img.getvalue()).decode('utf-8')
-
+    
     return render_template('roe.html', symbol=symbol, roe=roe, chart_base64=chart_base64)
 
 if __name__ == '__main__':
